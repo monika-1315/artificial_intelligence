28a9b5725f8f582ed193d3f19057000ca65c9e2b
Monika Marusiak <moniusiar@gmail.com>
2020-03-11 15:26:00 +0100

    Implemented mutation operator

--- GeneticAlgorithm/src/App.java
+++ GeneticAlgorithm/src/App.java
@@ -12,10 +12,10 @@ public class App {
 		GeneticAlgorithm galg= new GeneticAlgorithm(p, 5, 0.5, 0.5,2);
 		//galg.run();
 		galg.crossing(new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7,8,9,10)), new ArrayList<Integer>(Arrays.asList(10,9,8,7,6,5,4,3,2,1,0)));
-//		Algorithm alg = new Algorithm(p);
-//		alg.randomMethod(10000);
-//		alg.greedyAlg(7);
-//
+		Algorithm alg = new Algorithm(p);
+		alg.randomMethod(10000);
+		alg.greedyAlg(7);
+
 //		 load= new Loader("TSP/ali535.tsp");
 //		 p = load.load();
 //		 System.out.println(p.toString());
