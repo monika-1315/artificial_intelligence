b481059cc9731049bf9300bb98b2b84b4f8a5d97
Monika Marusiak <moniusiar@gmail.com>
2020-03-25 11:13:29 +0100

    checking domains

--- CSP/src/Jolka.java
+++ CSP/src/Jolka.java
@@ -4,17 +4,35 @@ import java.util.LinkedList;
 public class Jolka extends CSP<String> {
 
 	private ArrayList<String> words;
-	private ArrayList<Gap> fields;
+	private ArrayList<Gap>[] fields;
 
 	public Jolka(char[][] puzzle, ArrayList<String> words) {
 		this.initV = puzzle;
 		this.words = words;
-		fields = new ArrayList<Gap>();
-
+		fields = new ArrayList[2];
 		lookForFields(true);
 		lookForFields(false);
-		System.out.println(fields);
-		D = new LinkedList[1][1];
+		System.out.println(fields[0]);
+		System.out.println(fields[1]);
+		checkDomains();
+	}
+
+	private void checkDomains() {
+		D=new LinkedList[2][];
+		D[0]=new LinkedList[fields[0].size()];
+		D[1]=new LinkedList[fields[1].size()];
+		for (int ix=0; ix<2; ix++) {
+			for(int iy=0; iy<D[ix].length;iy++) {
+				D[ix][iy]=new LinkedList<String>();
+				for (String word: words) {
+					if(word.length()==fields[ix].get(iy).length) {
+						D[ix][iy].add(word);
+					}
+				}
+				System.out.println(ix+" "+iy+D[ix][iy]);
+			}
+		}//both hor and ver
+				
 	}
 
 	private void lookForFields(boolean isHorizontal) {
@@ -23,10 +41,12 @@ public class Jolka extends CSP<String> {
 		if(isHorizontal) {
 			x1Max=initV.length;
 			x2Max=initV[0].length;
+			fields[1]=new ArrayList<Gap>();
 		}
 		else {
 			x1Max=initV[0].length;
 			x2Max=initV.length;
+			fields[0]=new ArrayList<Gap>();
 		}
 		for (int x1 = 0; x1 < x1Max; x1++) {
 			for (int x2 = 0; x2 < x2Max; x2++) {
@@ -43,13 +63,19 @@ public class Jolka extends CSP<String> {
 					length++;
 				} else {
 					if (length > 1) {
-						fields.add(new Gap(row0, col0, length, isHorizontal));
+						if(isHorizontal)
+							fields[1].add(new Gap(row0, col0, length, isHorizontal));
+						else
+							fields[0].add(new Gap(row0, col0, length, isHorizontal));
 					}
 					length = 0;
 				}
 			} // for col
 			if (length > 1) {
-				fields.add(new Gap(row0, col0, length, isHorizontal));
+				if(isHorizontal)
+					fields[1].add(new Gap(row0, col0, length, isHorizontal));
+				else
+					fields[0].add(new Gap(row0, col0, length, isHorizontal));
 				length = 0;
 			}
 		} // for row
